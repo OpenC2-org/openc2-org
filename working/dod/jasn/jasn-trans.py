f32f84daa808cb60949050540de06acf96cb7fff
davaya <dpkemp@missi.ncsc.mil>
2016-09-07 18:07:12 -0400

    Schema validator for JASN

--- working/dod/jasn/jasn-trans.py
+++ working/dod/jasn/jasn-trans.py
@@ -1,67 +1,117 @@
 import json, jsonschema, re
 
-jasn_schema = """
-{   "type": "object",
+jasn_schema = {
+    "type": "object",
     "required": ["meta", "types"],
-    "additionalProperties": false,
+    "additionalProperties": False,
     "properties": {
         "meta": {
             "type": "object",
-            "additionalProperties": false,
+            "additionalProperties": False,
+            "required": ["targetNamespace"],
             "properties": {
+                "description": {"type": "string"},
+                "import": {
+                    "type": "object",
+                    "additionalProperties": False,
+                    "patternProperties": {
+                        "^\\S+$": {
+                            "type": "array",
+                            "additionalItems": False,
+                            "items": [
+                                {"type": "string"},
+                                {"type": "string"}
+                            ]
+                        }
+                    }
+                },
+                "root": {"type": "string"},
+                "sources": {
+                    "type": "object",
+                    "additionalProperties": False,
+                    "patternProperties": {
+                        "^\\w+$": {"type": "string"}
+                    }
+                },
                 "targetNamespace": {
                     "type": "array",
-                    "additionalitems": false,
+                    "additionalitems": False,
                     "items": [
                         {"type": "string"},
                         {"type": "string"}
                     ]
                 },
-                "version": {"type": "string"},
                 "title": {"type": "string"},
-                "description": {"type": "string"},
-                "root": {"type": "string"},
-                "import": {
-                    "type": "object",
-                    "additionalProperties": false,
-                    "patternProperties": {
-                        "<uri/uniqueid>": {
-                            "type": "array",
-                            "items": {"type": "string"}}}},
-                "sources": {
-                    "type": "object",
-                    "additionalProperties": false,
-                    "patternProperties": {
-                        "^\\w+$": {"type": "string"}}}}},
+                "version": {"type": "string"},
+            }
+        },
         "types": {
             "type": "array",
             "items": [
-                {"type": "string"},
-                {"type": "string"},
-                {"type": "string"},
-                {"type": "array",
-                "oneOf": [
-                    {   "type": "array",
-                        "minItems": 2,
-                        "maxItems": 2,
-                        "items": [
-                            {"type": "integer"},
-                            {"type": "string"}
-                        ]
-                    },
-                    {   "type": "array",
-                        "minItems": 4,
-                        "maxItems": 4,
-                        "items": [
-                            {"type": "integer"},
-                            {"type": "string"},
-                            {"type": "string"},
-                            {"type": "string"}
-                        ]
-                    }
-                ]
-            }]
+                {
+                    "type": "array",
+                    "additionalItems": False,
+                    "items": [
+                        {   "type": "string"},
+                        {   "type": "string"},
+                        {   "type": "string"},
+                        {   "type": "array",
+                            "items": {
+                                "oneOf": [
+                                    {   "type": "array",
+                                        "minItems": 2,
+                                        "maxItems": 2,
+                                        "items": [
+                                            {"type": "integer"},
+                                            {"type": "string"}
+                                        ]
+                                    },
+                                    {   "type": "array",
+                                        "minItems": 4,
+                                        "maxItems": 4,
+                                        "items": [
+                                            {"type": "integer"},
+                                            {"type": "string"},
+                                            {"type": "string"},
+                                            {"type": "string"}
+                                        ]
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                }
+            ]
         }
     }
 }
-"""
+
+def load_jasn(fname):
+    with open(fname) as f:
+        jasn = json.load(f)
+    jsonschema.Draft4Validator(jasn_schema).validate(jasn)
+    return jasn
+
+def dump(jasn):
+    for t in jasn["types"]:
+        tname, ttype, topts, tdata = t
+        print(tname + ": " + ttype + "(" + topts + ")")
+        for d in tdata:
+            print("  ", d)
+
+def to_python(jasn):
+    pass
+
+def to_pasn(jasn):
+    pass
+
+def to_tables(jasn):
+    pass
+
+def check(jasn):
+    pass
+
+if __name__ == '__main__':
+    jasn = load_jasn('cybox.jasn')
+    check(jasn)
+    dump(jasn)
