61f1fbc9b53bf18de3767156c43dc19d4e13d850
davaya <dpkemp@missi.ncsc.mil>
2016-09-09 16:11:18 -0400

    JASN translator

--- working/dod/jasn/jasn-trans.py
+++ working/dod/jasn/jasn-trans.py
@@ -1,4 +1,5 @@
 import json, jsonschema, re
+from textwrap import fill
 
 jasn_schema = {
     "type": "object",
@@ -8,7 +9,6 @@ jasn_schema = {
         "meta": {
             "type": "object",
             "additionalProperties": False,
-            "required": ["targetNamespace"],
             "properties": {
                 "description": {"type": "string"},
                 "import": {
@@ -30,61 +30,47 @@ jasn_schema = {
                         "^\\w+$": {"type": "string"}
                     }
                 },
-                "targetNamespace": {"type": "string"},
+                "namespace": {"type": "string"},
                 "title": {"type": "string"},
                 "version": {"type": "string"},
             }
         },
         "types": {
             "type": "array",
-            "items": [
-                {
-                    "type": "array",
-                    "additionalItems": False,
-                    "items": [
-                        {   "type": "string"},
-                        {   "type": "string"},
-                        {   "type": "string"},
-                        {   "type": "array",
-                            "items": {
-                                "oneOf": [
-                                    {   "type": "array",
-                                        "minItems": 2,
-                                        "maxItems": 2,
-                                        "items": [
-                                            {"type": "integer"},
-                                            {"type": "string"}
-                                        ]
-                                    },
-                                    {   "type": "array",
-                                        "minItems": 4,
-                                        "maxItems": 4,
-                                        "items": [
-                                            {"type": "integer"},
-                                            {"type": "string"},
-                                            {"type": "string"},
-                                            {"type": "string"}
-                                        ]
-                                    }
-                                ]
-                            }
+            "items": {
+                "type": "array",
+                "additionalItems": False,
+                "items": [
+                    {   "type": "string"},
+                    {   "type": "string"},
+                    {   "type": "string"},
+                    {   "type": "array",
+                        "items": {
+                            "type": "array",
+                            "minItems": 2,
+                            "maxItems": 4,
+                            "items": [
+                                {"type": "integer"},
+                                {"type": "string"},
+                                {"type": "string"},
+                                {"type": "string"}
+                            ]
                         }
-                    ]
-                }
-            ]
+                    }
+                ]
+            }
         }
     }
 }
 
 def jasn_check(jasn):
     jsonschema.Draft4Validator(jasn_schema).validate(jasn)
-    for t in jasn["types"]:
-        if t[3]:
-            tmax = max([len(i) for i in t[3]])
-            tmin = min([len(i) for i in t[3]])
-            if tmin != tmax or tmax != (2 if t[1].lower() == 'enumerated' else 4):
-                print("Item length error:", t[0], t[1], t[2], tmin, tmax)
-    return jasn
+    for t in jasn["types"]:     # datatype definition: 0-name, 1-type, 2-options, 3-item list
+        n = 2 if t[1].lower() == "enumerated" else 4
+        for i in t[3]:          # item definition: 0-tag, 1-name, 2-type, 3-options
+            if len(i) != n:
+                print("Item format error:", t[0], t[1], i[1], "-", len(i), "!=", n)
+    return jasn                 # TODO: check tag collisions
 
 def jasn_load(fname):
     with open(fname) as f:
@@ -92,21 +78,53 @@ def jasn_load(fname):
     jasn_check(jasn)
     return jasn
 
-def jasn_dumps(jasn, fname):
+def jasn_dumps(jasn):
     return json.dumps(jasn, indent=2)
 
-def pasn_dumps(jasn, fname):
-    pasn = ""
+def jasn_dump(jasn, fname):
+    with open(fname, "w") as f:
+        f.write(jasn_dumps(jasn))
+
+def pasn_dumps(jasn):
+    pasn = "/*\n"
+    hdrs = jasn["meta"]
+    hlist = ["title", "version", "description", "namespace", "root", "import", "sources"]
+    for h in hlist + list(set(hdrs) - set(hlist)):
+        if h in hdrs:
+            if h == "description":
+                pasn += fill(hdrs[h], width=80, initial_indent="{0:14} ".format(h + ":"), subsequent_indent=15*" ") + "\n"
+            elif h == "import" or h == "sources":
+                hh = "{:14} ".format(h + ":")
+                for k, v in hdrs[h].items():
+                    pasn += hh + k + ": " + v + "\n"
+                    hh = 15*" "
+            elif h == "root":
+                pasn += "{0:14} Root type: {1:}\n".format(h + ":", hdrs[h][0])
+                pasn += 15*" " + "Default ns: " + hdrs[h][1] + "\n"
+            else:
+                pasn += "{0:14} {1:}\n".format(h + ":", hdrs[h])
+    pasn += "*/\n"
+
+    asn1type = {"String": "UTF8STRING", "Integer": "INTEGER", "Boolean": "BOOLEAN"}
     for t in jasn["types"]:
         tname, ttype, topts, titems = t
+        for i in titems:
+            if len(i) > 2:
+                if i[2] in asn1type:        # Translate primitive types to ASN.1
+                    i[2] = asn1type[i[2]]
         pasn += "\n" + tname + " ::= " + ttype.upper() + ("(" + topts + ")" if topts else "") + " {\n"
         flen = min(32, max(12, max([len(i[1]) for i in titems], default=0)))    # TODO: No default in Python 2
         if ttype.lower() == "enumerated":
             fmt = "    {1:" + str(flen) + "} ({0:d})"
         else:
             fmt = "    {1:" + str(flen) + "} [{0:d}] {2}{3}"
-        pasn += ",\n".join([fmt.format(*i) for i in titems]) + "\n}\n"
+        pasn += ",\n".join([fmt.format(*i) for i in titems])
+        pasn += ("\n}\n" if titems else "}\n")
     return pasn
+
+def pasn_dump(jasn, fname):
+    with open(fname, "w") as f:
+        f.write(pasn_dumps(jasn))
 
 def python_dumps(jasn, fname):
     pass
@@ -117,5 +135,5 @@ def tables_dumps(jasn, fname):
 if __name__ == "__main__":
     fname = "cybox"
     jasn = jasn_load(fname + ".jasn")
-    print(pasn_dumps(jasn, fname + ".pasn"))
-    print(jasn_dumps(jasn, fname + "_saved.jasn"))
+    pasn_dump(jasn, fname + "_gen.pasn")
+    jasn_dump(jasn, fname + "_gen.jasn")
